b7b11a31ff4fecaccc64b8544e348485bfacc447
Vika Shcherbak <vikashcherbaka@gmail.com>
2023-03-06 00:39:29 +0100

    change the ear, nav, bg

--- ohr-frontend/src/App.js
+++ ohr-frontend/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import ear from './accets/logo2.png';
+import avatar from './accets/avatar.png';;
 
 function App() {
 
@@ -6,16 +8,13 @@ function App() {
     <div className="App">
       <div className="navbar bg-base-100">
         <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">ohr</a>
+          <a className="btn btn-ghost normal-case text-xl">bla</a>
         </div>
         <div className="flex-none gap-2">
-          {/* <div className="form-control">
-            <input type="text" placeholder="Search" className="input input-bordered" />
-          </div> */}
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <img src="" />
+                <img src={avatar} />
               </div>
             </label>
             <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
@@ -30,7 +29,22 @@ function App() {
           </div>
         </div>
       </div>
-      <h1>ohr</h1>
+
+      <div className="l">
+        <h1>ohr</h1>
+        <img className="listen" src={ear} />
+      </div>
+
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">bla</a>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="flex-1">
+            <a className="btn btn-ghost normal-case text-xl">bla</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
