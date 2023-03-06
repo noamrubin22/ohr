66f73ab75aefca8e278897182562e8a120cefa37
Vika Shcherbak <vikashcherbaka@gmail.com>
2023-03-06 12:48:44 +0100

    add LOG IN button

--- ohr-frontend/src/App.js
+++ ohr-frontend/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import ear from './accets/logo2.png';
-import avatar from './accets/avatar.png';;
+import avatar from './accets/avatar.png';
 
 function App() {
-
   return (
     <div className="App">
+      {/* upper nav, will be moved (probably) in it's own component */}
       <div className="navbar bg-base-100">
         <div className="flex-1">
           <a className="btn btn-ghost normal-case text-xl">bla</a>
@@ -14,14 +14,13 @@ function App() {
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
+                 {/* avatar should be the wallet picture */}
                 <img src={avatar} />
               </div>
             </label>
             <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
               <li>
-                <a className="justify-between">
-                  Profile
-                </a>
+                <a className="justify-between">Profile</a>
               </li>
               <li><a>Settings</a></li>
               <li><a>Logout</a></li>
@@ -30,11 +29,16 @@ function App() {
         </div>
       </div>
 
+      {/*  here is the listen/record button*/}
       <div className="l">
         <h1>ohr</h1>
         <img className="listen" src={ear} />
       </div>
 
+      {/* here is the log in button!!!!! */}
+      <button className="btn btn-active">LOG IN</button>
+
+      {/*bottom nav (will be moved (probably) in it's ovn component) */}
       <div className="navbar bg-base-100">
         <div className="flex-1">
           <a className="btn btn-ghost normal-case text-xl">bla</a>
---
+++ ohr-frontend/src/components/UpperNav.jsx
@@ -0,0 +1,31 @@
+//import avatar from '../accets/avatar.png';
+
+// function UpperNav() {
+//     return (
+//         <div className="navbar bg-base-100">
+//             <div className="flex-1">
+//                 <a className="btn btn-ghost normal-case text-xl">bla</a>
+//             </div>
+//             <div className="flex-none gap-2">
+//                 <div className="dropdown dropdown-end">
+//                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+//                         <div className="w-10 rounded-full">
+//                             <img src="" />
+//                         </div>
+//                     </label>
+//                     <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+//                         <li>
+//                             <a className="justify-between">
+//                                 Profile
+//                             </a>
+//                         </li>
+//                         <li><a>Settings</a></li>
+//                         <li><a>Logout</a></li>
+//                     </ul>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default UpperNav;
