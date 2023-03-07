220f4952300efaabdfb51bf0f0f918492fff14a6
Vika Shcherbak <vikashcherbaka@gmail.com>
2023-03-07 10:56:50 +0100

    base + wallet merge

--- ohr-frontend/src/components/Landing.jsx
+++ ohr-frontend/src/components/Landing.jsx
@@ -2,7 +2,7 @@ function Landing({ear}) {
     return (
         <>
             <div className="middle-of-the-screen">
-                <h1>ohr</h1>
+                <h1 className='app-title'>ohr</h1>
                 <img className="listen" src={ear} />
             </div>
         </>
