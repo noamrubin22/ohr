9196c29ffdf119aa86fa98a3e523853bb0958526
NinoGlonti <glonti.nin@gmail.com>
2023-03-12 12:19:36 +0400

    arweave upload

--- ohr-frontend/src/components/visualisation-view.jsx
+++ ohr-frontend/src/components/visualisation-view.jsx
@@ -3,6 +3,9 @@ import { WalletAdapterNetwork, WalletNotConnectedError } from '@solana/wallet-ad
 import { clusterApiUrl, Transaction, SystemProgram, Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
 
 import { ConnectionProvider, WalletProvider, useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { useEffect } from 'react';
+import env from "react-dotenv";
+
 let thelamports = 0;
 let theWallet = "9m5kFDqgpf7Ckzbox91RYcADqcmvxW4MmuNvroD5H2r9"
 
@@ -13,19 +16,14 @@ function Visualisation({ setVisualisationView, blob }) {
 
     const bs58 = require('bs58');
     
-
-
-
     async function onClick()  {
 
         if (!publicKey) throw new WalletNotConnectedError();
         connection.getBalance(publicKey).then((bal) => {
             console.log(bal/LAMPORTS_PER_SOL, "lamp sol");
-
         });
-
-        
-        let firstWinPrivKey = [190,149,19,132,242,8,56,13,87,220,241,9,100,135,215,185,7,51,10,139,36,70,158,107,193,211,187,237,150,233,215,215,251,145,57,144,236,181,148,83,75,40,200,152,20,20,230,154,237,242,177,74,235,238,88,212,233,193,45,131,180,221,134,63].slice(0,32);
+        let key = env.SOLANA_PRIVATE_KEY;
+        let firstWinPrivKey = key.slice(0,32);
         let secretKey = web3.Keypair.fromSeed(Uint8Array.from(firstWinPrivKey));
       
         const mintNFTResponse = await actions.mintNFT({
@@ -33,9 +31,8 @@ function Visualisation({ setVisualisationView, blob }) {
             wallet: new NodeWallet(Keypair.fromSecretKey(secretKey.secretKey)),
             uri: 'https://www.arweave.net/1r-ImuiIxFl18UQolAoBnwLDMVcjkVAHruhtsaBpA7U?ext=json',
             maxSupply: 1
-          });
+          }).catch(e=>console.error(e, "erroooooor"));
     }
-
 
     async function getPCM(b) {
         const url = URL.createObjectURL(b);
@@ -44,7 +41,6 @@ function Visualisation({ setVisualisationView, blob }) {
         const arrayBuffer = await response.arrayBuffer();
         const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
         const pcm = audioBuffer.getChannelData(0);
-        //console.log(pcm, "vika console");
     }
     getPCM(blob);
 
