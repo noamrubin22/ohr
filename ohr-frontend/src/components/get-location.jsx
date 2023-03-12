5ac23df9e1a628f92d1566edbe5d6f42d861230d
Vika Shcherbak <vikashcherbaka@gmail.com>
2023-03-12 01:44:47 +0100

    ear pulsating

--- ohr-frontend/src/components/get-location.jsx
+++ ohr-frontend/src/components/get-location.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const GetLocation = ({x, y, xx, yy}) => {
+const GetLocation = ({ x, y, xx, yy }) => {
 
     // const [latitude, setLatitude] = useState(null);
     // const [longitude, setLongitude] = useState(null);
@@ -23,8 +23,17 @@ const GetLocation = ({x, y, xx, yy}) => {
 
     return (
         <div>
-            {xx && yy ? <p>Your location is: {xx}, {yy}</p> : null}
-            <button className="btn" onClick={getLocation}>+ location</button>
+
+            {/* <button tabIndex={0} className="btn btn-ghost" onClick={getLocation}>+ location</button> */}
+
+            <div className="dropdown dropdown-left dropdown-end">
+                <button tabIndex={0} className="btn btn-ghost blue" onClick={getLocation}>+ location</button>
+                {xx && yy ? <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                    <li> {<p>Location {xx}, {yy} was added.</p>}</li>
+                </ul> : null }
+
+
+            </div>
         </div>
     );
 }
