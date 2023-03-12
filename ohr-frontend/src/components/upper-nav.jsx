a915631b798b83c2e228b0da160f54d3e9419c49
NinoGlonti <glonti.nin@gmail.com>
2023-03-12 20:23:08 +0400

    fix two modal errors

--- ohr-frontend/src/components/upper-nav.jsx
+++ ohr-frontend/src/components/upper-nav.jsx
@@ -6,9 +6,9 @@ function UpperNav() {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <label htmlFor="my-modal-4" className="btn btn-ghost normal-case text-xl">NFT map</label>
-                <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-                <label htmlFor="my-modal-4" className="modal cursor-pointer">
+                <label htmlFor="my-modal-5" className="btn btn-ghost normal-case text-xl">NFT map</label>
+                <input type="checkbox" id="my-modal-5" className="modal-toggle" />
+                <label htmlFor="my-modal-5" className="modal cursor-pointer">
                     <label className="relative" htmlFor="">
                         <Map />
                     </label>
