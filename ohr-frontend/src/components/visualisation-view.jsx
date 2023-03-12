9451bb66f6f2b86c64da753b1fe60c81c39fedd3
Viki <viktoriacseter@gmail.com>
2023-03-12 10:18:48 +0100

    visualization add pixels

--- ohr-frontend/src/components/visualisation-view.jsx
+++ ohr-frontend/src/components/visualisation-view.jsx
@@ -1,21 +1,12 @@
 import GetLocation from "./get-location";
-<<<<<<< HEAD
 import { React, useState, useRef, useEffect } from "react";
-=======
-import { useState } from "react";
->>>>>>> 4b1f892af600024c0201848e64ed56fc19080452
 import { actions, utils, programs, NodeWallet } from '@metaplex/js';
 import { WalletAdapterNetwork, WalletNotConnectedError } from '@solana/wallet-adapter-base';
 import { clusterApiUrl, Transaction, SystemProgram, Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
 import { ConnectionProvider, WalletProvider, useConnection, useWallet } from '@solana/wallet-adapter-react';
-<<<<<<< HEAD
 //import CanvasVisual from "./canvas";
 import '../App.css'
-
-=======
-import { useEffect } from 'react';
 import env from "react-dotenv";
->>>>>>> 4b1f892af600024c0201848e64ed56fc19080452
 
 let thelamports = 0;
 let theWallet = "9m5kFDqgpf7Ckzbox91RYcADqcmvxW4MmuNvroD5H2r9";
@@ -29,8 +20,7 @@ const VisualisationAndCoords = ({ setVisualisationView, blob }) => {
     const { publicKey, sendTransaction } = useWallet();
     const web3 = require("@solana/web3.js");
     const bs58 = require('bs58');
-<<<<<<< HEAD
-    // track the color from pcm3
+    // track the color from pcm
     const [color1, setColor1] = useState("");
     const [color2, setColor2] = useState("");
     const [color3, setColor3] = useState("");
@@ -45,21 +35,12 @@ const VisualisationAndCoords = ({ setVisualisationView, blob }) => {
 
     async function onClick() {
 
-=======
-    
-    async function onClick()  {
->>>>>>> 4b1f892af600024c0201848e64ed56fc19080452
         if (!publicKey) throw new WalletNotConnectedError();
         connection.getBalance(publicKey).then((bal) => {
             console.log(bal / LAMPORTS_PER_SOL, "lamp sol");
         });
-<<<<<<< HEAD
-
-        let firstWinPrivKey = [190, 149, 19, 132, 242, 8, 56, 13, 87, 220, 241, 9, 100, 135, 215, 185, 7, 51, 10, 139, 36, 70, 158, 107, 193, 211, 187, 237, 150, 233, 215, 215, 251, 145, 57, 144, 236, 181, 148, 83, 75, 40, 200, 152, 20, 20, 230, 154, 237, 242, 177, 74, 235, 238, 88, 212, 233, 193, 45, 131, 180, 221, 134, 63].slice(0, 32);
-=======
         let key = env.SOLANA_PRIVATE_KEY;
-        let firstWinPrivKey = key.slice(0,32);
->>>>>>> 4b1f892af600024c0201848e64ed56fc19080452
+        let firstWinPrivKey = key.slice(0, 32);
         let secretKey = web3.Keypair.fromSeed(Uint8Array.from(firstWinPrivKey));
 
         const mintNFTResponse = await actions.mintNFT({
@@ -67,14 +48,9 @@ const VisualisationAndCoords = ({ setVisualisationView, blob }) => {
             wallet: new NodeWallet(Keypair.fromSecretKey(secretKey.secretKey)),
             uri: 'https://www.arweave.net/1r-ImuiIxFl18UQolAoBnwLDMVcjkVAHruhtsaBpA7U?ext=json',
             maxSupply: 1
-<<<<<<< HEAD
-        });
-    }
-=======
-          }).catch(e=>console.error(e,));
-        };
-    
->>>>>>> 4b1f892af600024c0201848e64ed56fc19080452
+        }).catch(e => console.error(e,));
+    };
+
 
     async function getPCM(b) {
 
@@ -83,10 +59,7 @@ const VisualisationAndCoords = ({ setVisualisationView, blob }) => {
         const response = await fetch(url);
         const arrayBuffer = await response.arrayBuffer();
         const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-<<<<<<< HEAD
-=======
         console.log(audioBuffer);
->>>>>>> 4b1f892af600024c0201848e64ed56fc19080452
         const pcm = audioBuffer.getChannelData(0);
 
         const pcm01 = pcm[10000].toString();
@@ -220,7 +193,6 @@ const VisualisationAndCoords = ({ setVisualisationView, blob }) => {
 
     return (
         <div className="central-inner-container">
-<<<<<<< HEAD
             <div className="CanvasVisual">
                 <canvas ref={canvas} height="400" width="500">
                 </canvas>
@@ -229,15 +201,8 @@ const VisualisationAndCoords = ({ setVisualisationView, blob }) => {
             <GetLocation x={setLatitude} y={setLongitude} xx={latitude} yy={longitude} />
             <button className="btn" onClick={onClick}>mint NFT</button>
             <button className="btn" onClick={handleBack}>back</button>
-=======
-            <div className="vis-btns">
-                <GetLocation x={setLatitude} y={setLongitude} xx={latitude} yy={longitude} />
-                <button className="btn btn-ghost big" onClick={(e) => console.log("minting will work soon")}>mint NFT</button>
-                <button className="btn btn-ghost" onClick={handleBack}>back</button>
-            </div>
->>>>>>> 4b1f892af600024c0201848e64ed56fc19080452
-        </div>
+        </div >
     )
 
-    }
+}
 export default VisualisationAndCoords;
