23a28d76d8bbb7cd4a9150b9375b63b16fe1bd25
Vika Shcherbak <vikashcherbaka@gmail.com>
2023-03-29 10:42:48 +0200

    css mobile view fixes

--- ohr-frontend/src/components/upload-data-to-arweave.jsx
+++ ohr-frontend/src/components/upload-data-to-arweave.jsx
@@ -1,8 +1,16 @@
 import { React, useState, useEffect } from "react";
+
 import Arweave from 'arweave';
+import { actions } from '@metaplex/js';
+import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
+import { clusterApiUrl, LAMPORTS_PER_SOL, Connection } from '@solana/web3.js';
+import { useWallet } from '@solana/wallet-adapter-react';
 const { Buffer } = require("buffer");
 
 const UploadDataToArweave = ({ blob }) => {
+
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+    const wallet = useWallet();
     const arweaveKey = JSON.parse(process.env.ARWEAVE_KEY);
     const arweave = Arweave.init({
         host: 'arweave.net',
@@ -52,11 +60,50 @@ const UploadDataToArweave = ({ blob }) => {
 
         // we are going to use dataUrl to create metadata (create-metadata-and-mint.jsx)
         const dataUrl = transaction.id ? `https://arweave.net/${transaction.id}` : undefined;
+
         console.log(dataUrl, 'DATA UPLOADED');
+        let metadata = `{
+            "name":"øhr",
+            "symbol":"NFT",
+            "description":"bla bla bla",
+            "seller_fee_basis_points":100,
+            "image":"https://arweave.net/${transaction.id}",
+            "attributes":[{"trait_type":"Ear","value":"Classic"}],
+            "external_url":"",
+            "properties":
+                {"files":[{"uri":"https://arweave.net/${transaction.id}","type":"image/png"}],
+                "category":"image",
+                "creators":[{"address":"CyVTvTSEYWv9LQHwmR3w69zVPMBZWJxdAahYp6JqFfkp","verified":true,"share":100}]}
+            }`
+
+        metadata = metadata.trim();
+        const metadataRequest = JSON.parse(JSON.stringify(metadata));
+        console.log(metadataRequest, "METADATA");
+        if (metadataRequest === null) return;
+
+        const metadataTransaction = await arweave.createTransaction(
+            { data: metadataRequest },
+            arweaveKey
+        );
+        metadataTransaction.addTag('Content-Type', 'application/json');
+        await arweave.transactions.sign(metadataTransaction, arweaveKey);
+
+        // the uri
+        console.log(`https://arweave.net/${metadataTransaction.id}`);
+
+        let res = await arweave.transactions.post(metadataTransaction);
+        console.log(res);
+
+        const mintNFTResponse = await actions.mintNFT({
+            connection,
+            wallet: wallet,
+            uri: `https://arweave.net/${metadataTransaction.id}`,
+            maxSupply: 1
+        }).catch(e => console.error(e,));
     };
 
     return (
-        <button className="btn btn-ghost" onClick={onClick}>1. upload data to Arweave</button>
+        <button className="btn btn-ghost" onClick={onClick}>mint</button>
     );
 }
 
