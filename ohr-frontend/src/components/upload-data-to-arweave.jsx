7432b010921e2d395d0908e75891f76ba558a2be
NinoGlonti <glonti.nin@gmail.com>
2023-03-29 15:31:19 +0400

    upload audio and img on arweave and mint them

--- ohr-frontend/src/components/upload-data-to-arweave.jsx
+++ ohr-frontend/src/components/upload-data-to-arweave.jsx
@@ -1,62 +1,75 @@
 import { React, useState, useEffect } from "react";
 import Arweave from 'arweave';
+import CreateMetaAndMint from "./create-metadata-and-mint";
+
 const { Buffer } = require("buffer");
 
 const UploadDataToArweave = ({ blob }) => {
     const arweaveKey = JSON.parse(process.env.ARWEAVE_KEY);
+
     const arweave = Arweave.init({
         host: 'arweave.net',
         port: 443,
         protocol: 'https',
     });
 
-    //const [audioBuffer, setAudioBuffer] = useState(null);
+    const [audioBuffer, setAudioBuffer] = useState(null);
     const [imageBuffer, setImageBuffer] = useState(null);
+    const [imgUrl, setImgUrl] = useState("");
+    const [audioUrl, setAudioUrl] = useState("");
 
-    // useEffect(() => {
-    //     async function getBuffer(b) {
-    //         const url = URL.createObjectURL(b);
-    //         console.log(url);
-    //         const response = await fetch(url);
-    //         const arrayBuffer = await response.arrayBuffer();
-    //         const buffer = Buffer.from(arrayBuffer);
-    //         setAudioBuffer(buffer);
-    //     }
-    //     getBuffer(blob);
-    // }, []);
+     useEffect(() => {
+         async function getBuffer(b) {
+             const url = URL.createObjectURL(b);
+             const response = await fetch(url);
+             const arrayBuffer = await response.arrayBuffer();
+             const buffer = Buffer.from(arrayBuffer);
+             setAudioBuffer(buffer);
+          }
+         getBuffer(blob);
+     }, []);
 
     useEffect(() => {
         const loadImage = async () => {
             const response = await fetch(require("../assets/smallEar.png"));
-            //console.log(response, "response");
             const arrayBuffer = await response.arrayBuffer();
-            //console.log(arrayBuffer, "arrayBuffer");
             const buffer = Buffer.from(arrayBuffer);
             setImageBuffer(buffer);
         }
         loadImage();
     }, []);
-
-    // if uploading an audio replace all imageBuffer to audioBuffer
+    
+    //creating two arweave transactions on one click
     async function onClick() {
-        if (imageBuffer === null) return;
-        console.log(imageBuffer, "BUFFER");
-
+       // if (imageBuffer === null) return;
+       
         let transaction = await arweave.createTransaction({ data: imageBuffer });
-        //transaction.addTag('Content-Type', 'audio/wav');
+        let transaction2 = await arweave.createTransaction({ data: audioBuffer});
+       // let uploader = await arweave.transactions.getUploader(transaction);
+
         transaction.addTag('Content-Type', 'image/img');
+        transaction2.addTag('Content-Type', 'audio/wav');
 
         await arweave.transactions.sign(transaction, arweaveKey);
-        const response = await arweave.transactions.post(transaction);
-        console.log(response);
+        await arweave.transactions.sign(transaction2, arweaveKey);
 
-        // we are going to use dataUrl to create metadata (create-metadata-and-mint.jsx)
+        const response = await arweave.transactions.post(transaction);
+        const responseAudio = await arweave.transactions.post(transaction2);
+
+
         const dataUrl = transaction.id ? `https://arweave.net/${transaction.id}` : undefined;
-        console.log(dataUrl, 'DATA UPLOADED');
+        const dataUrlAudio = transaction2.id ? `https://arweave.net/${transaction2.id}` : undefined;
+        
+        setImgUrl(dataUrl);
+        setAudioUrl(dataUrlAudio);
     };
 
     return (
-        <button className="btn btn-ghost" onClick={onClick}>1. upload data to Arweave</button>
+      <>
+        <button className="btn btn-ghost" onClick={onClick}>1. Upload data to Arweave</button>
+        <CreateMetaAndMint imgUrl={imgUrl} audioUrl={audioUrl}/>
+      </>
+
     );
 }
 
